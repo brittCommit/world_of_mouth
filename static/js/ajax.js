91cbc86c4444673bf223eb23390acdf6b115e977
Brittney Huttner <brittneyhuttner@gmail.com>
2020-06-13 20:22:40 +0000

    added form to add stop to exsisting trip, still need to link to db

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,18 +1,18 @@
 "use strict";
 
 
-    <script>
-    $('#new-trip').on('click', (evt) => {
-    evt.preventDefault();
+    // <script>
+    // $('#new-trip').on('click', (evt) => {
+    // evt.preventDefault();
 
-    alert("it worked!")
+    // alert("it worked!")
     
-    const selectedValue = $('#new-trip').text();
-    console.log(selectedValue)
+    // const selectedValue = $('#new-trip').text();
+    // console.log(selectedValue)
 
-    $.get(`/create_route`, {'email', }(res) => {
-    $('#new-trip').text(res.trip_description);
-    $('#').html(res.stay_length);
-    })
-    });
-    </script>
+    // $.get(`/create_route`, {'email', }(res) => {
+    // $('#new-trip').text(res.trip_description);
+    // $('#').html(res.stay_length);
+    // })
+    // });
+    // </script>
