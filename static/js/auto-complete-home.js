050708bec4cb610d1848b1215be0b51bd98914ff
Brittney Huttner <brittneyhuttner@gmail.com>
2020-06-25 19:08:17 +0000

    fixed starting city so it comes back with value

--- static/js/auto-complete-home.js
+++ static/js/auto-complete-home.js
@@ -1,9 +1,16 @@
+let STARTCITY = '';
+let ENDCITY = '';
+let COUNTRY = '';
+
 var placeSearch, autocomplete;
   var componentFormMain = {
-    country: 'short_name',
+    country: 'short_name'
   };
-  var componentForm = {
-    locality: 'long_name',
+  var componentFormStart = {
+    locality: 'long_name'
+  }
+  var componentFormEnd = {
+    locality: 'long_name'
   }
 
 function initAutocomplete() {
@@ -28,11 +35,11 @@ function initAutocomplete() {
   autocompleteIsEnd = new google.maps.places.Autocomplete(
     /** @type {!HTMLInputElement} */(document.getElementById('autocompleteIsEnd')),
     {types: ['geocode']});
-
+ 
   // When the user selects an address from the dropdown, populate the address
   // fields in the form.
   autocompleteIsEnd.addListener('place_changed', fillInAddressIsEnd);
-}
+
 
 function fillInAddress() {
   // Get the place details from the autocomplete object.
@@ -47,17 +54,8 @@ function fillInAddress() {
     var addressType = key;
     if (componentFormMain[addressType]) {
       if (componentFormMain[addressType])
-      var val = trip[key];
-      console.log(val)
-
-  $('#get-trips').on('submit', (evt) => {
-    evt.preventDefault();
-  
-  const formInputs = {
-    country: val
-  };
-  window.location.href = `/view_routes/${country}`
-});
+      COUNTRY = trip[key];
+      console.log(COUNTRY)
 };
 }
 }
@@ -66,17 +64,27 @@ function fillInAddressIsStart() {
   // Get the place details from the autocomplete object.
   var place = autocompleteIsStart.getPlace();
   var address_components = place.address_components
+  console.log(address_components)
   var locality = place.address_components[0].long_name
-  var trip = {locality};
+  var country = place.address_components[address_components.length -1].short_name
+  var lat = place.geometry.location.lat()
+  var lng = place.geometry.location.lng()
+  console.log(country)
+  var trip = {
+    locality
+  };
   console.log(trip)
 
   tripKeys=Object.keys(trip);
+  console.log(tripKeys)
   for (var key of tripKeys) {
     var addressType = key;
-    if (componentForm[addressType]) {
-      if (componentForm[addressType])
-      var val = tripKeys[key];
-      console.log(val)
+    console.log(addressType)
+    if (componentFormStart[addressType]) {
+      if (componentFormStart[addressType])
+        console.log(componentFormStart[addressType])
+      STARTCITY = trip[key];
+      console.log(STARTCITY)
 };
 }
 }
@@ -97,15 +105,35 @@ function fillInAddressIsEnd() {
   console.log(trip)
 
   tripKeys=Object.keys(trip);
+  console.log(tripKeys)
   for (var key of tripKeys) {
     var addressType = key;
-    if (componentForm[addressType]) {
-      if (componentForm[addressType])
-      var val = trip[key];
-      console.log(val)
+    console.log(addressType)
+    if (componentFormEnd[addressType]) {
+      if (componentFormEnd[addressType])
+        console.log(componentFormEnd[addressType])
+      ENDCITY = trip[key];
+      console.log(ENDCITY)
 };
 }
 }
+
+  $('#get-trips').on('submit', (evt) => {
+    evt.preventDefault();
+  
+   const formValues = {
+      country: COUNTRY,
+      start_city_name: STARTCITY,
+      end_city_name: ENDCITY
+  };
+
+  $.post(`/view_routes/${COUNTRY}`, formValues,(res)=> {
+    console.log(formValues)
+    });
+
+  window.location.href = `/view_routes/${COUNTRY}`
+  });
+  };
 
 // Bias the autocomplete object to the user's geographical location,
 // as supplied by the browser's 'navigator.geolocation' object.
@@ -122,5 +150,6 @@ function geolocate() {
       });
       autocomplete.setBounds(circle.getBounds());
     });
-  }
-}
+  };
+};
+
